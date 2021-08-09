55acc9618cca07dd9ec9d6422ca7721e4553538c
Swati Patel <dvswati09@gmail.com>
2021-08-09 12:18:28 +0530

    Division

---
+++ div.java
@@ -0,0 +1,10 @@
+class div
+{
+    public static void main(String args[])
+    {
+        int a=9,b=9;
+        System.out.println("div=" + (a/b));
+        System.out.println("Hello mini");
+
+    }
+}
